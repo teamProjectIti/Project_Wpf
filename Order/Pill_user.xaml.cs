bd67bc9b9d243d779a18c0b0c629c15913e7c927
Beshoy Amen <beshoyamen5@gmail.com>
2022-02-19 14:13:02 +0200

    add first comment

---
+++ Order/Pill_user.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_iti_wpf_market.Order
+{
+    /// <summary>
+    /// Interaction logic for Pill_user.xaml
+    /// </summary>
+    public partial class Pill_user : Window
+    {
+        public Pill_user()
+        {
+            InitializeComponent();
+        }
+    }
+}
