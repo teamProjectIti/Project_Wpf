1be6e229951d104455cf301124ff7c43588e7398
Beshoy Amen <beshoyamen5@gmail.com>
2022-02-19 14:24:49 +0200

    add model for pill

---
+++ Model/Pill_SuplieriesClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_iti_wpf_market.Model
+{
+    internal class Pill_SuplieriesClass
+    {
+        public int ID_bill { get; set; }
+        public int Qountity { get; set; }
+        public double Sum_Total { get; set; }
+        public double price { get; set; }
+        public string proname { get; set; }
+        public string suppname { get; set; }
+        public string catoname { get; set; }
+        public DateTime date_Bill { get; set; }
+
+        public Pill_SuplieriesClass(int iD_bill, double price, int qountity, double sum_Total, string proname, string suppname, string catoname, DateTime date_bill)
+        {
+            ID_bill = iD_bill;
+            this.price = price;
+            Qountity = qountity;
+            Sum_Total = sum_Total;
+            this.proname = proname;
+            this.suppname = suppname;
+            this.catoname = catoname;
+            this.date_Bill = date_bill;
+        }
+    }
+}
---
+++ Model/Pill_userClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_iti_wpf_market.Model
+{
+    internal class Pill_userClass
+    {
+        public int ID_bill { get; set; }
+        public double price { get; set; }
+        public string name_product { get; set; }
+        public int Qountity { get; set; }
+        public double Sum_Total { get; set; }
+        public int Descound { get; set; }
+        public string Details_Bill { get; set; }
+        public string Name_Caher { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime date_Bill { get; set; }
+
+        public Pill_userClass(int iD_bill, double price, int qountity, double sum_Total, int descound, string details_Bill, string name_Caher, string name, string email, string phone, DateTime date_bill)
+        {
+            ID_bill = iD_bill;
+            this.price = price;
+            Qountity = qountity;
+            Sum_Total = sum_Total;
+            Descound = descound;
+            Details_Bill = details_Bill;
+            Name_Caher = name_Caher;
+            Name = name;
+            Email = email;
+            Phone = phone;
+            this.date_Bill = date_bill;
+        }
+
+        public Pill_userClass()
+        {
+        }
+    }
+}
