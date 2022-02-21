6cedab47523a68e0a3335900eb9d6b976a2d3b60
Beshoy Amen <beshoyamen5@gmail.com>
2022-02-21 13:47:52 +0200

    add comment data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using project_iti_wpf_market.Order;
+using project_iti_wpf_market.security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,57 @@ namespace project_iti_wpf_market
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 f = new Window1();
+            f.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MarketProduct f = new MarketProduct();
+            f.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Users f = new Users();
+            f.ShowDialog();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            supplier f = new supplier();
+            f.ShowDialog();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Window2 f = new Window2();
+            f.ShowDialog();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Pill_spuliers ss = new Pill_spuliers();
+            ss.ShowDialog();
+
+
+          
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Pill_user s = new Pill_user();
+            s.ShowDialog();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            login l = new login();
+            l.ShowDialog();
         }
     }
 }
